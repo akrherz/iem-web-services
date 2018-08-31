85321f797f19cdc60f8645a36ccb1b47699c9aa9
akrherz <akrherz@iastate.edu>
2018-08-30 23:18:17 -0500

    use logger provided timestamp

--- pylib/services/meteobridge.py
+++ pylib/services/meteobridge.py
@@ -4,7 +4,7 @@ import sys
 import datetime
 
 import pytz
-from pyiem.util import get_properties, get_dbconn
+from pyiem.util import get_properties, get_dbconn, utc
 from pyiem.observation import Observation
 
 PROPS = {}
@@ -23,8 +23,13 @@ def handler(version, fields, environ):
     sid = ('OT0013'
            if PROPS.get('meteobridge.key.OT0013') == fields.get('key')
            else 'OT0014')
-    now = datetime.datetime.utcnow()
-    now = now.replace(tzinfo=pytz.UTC)
+    if len(fields.get('time', '')) == 14:
+        _t = fields.get('time')
+        now = utc(int(_t[:4]), int(_t[4:6]), int(_t[6:8]), int(_t[8:10]),
+                  int(_t[10:12]), int(_t[12:14]))
+    else:
+        now = datetime.datetime.utcnow()
+        now = now.replace(tzinfo=pytz.UTC)
     ob = Observation(sid, 'OT', now)
     for fname in ['tmpf', 'max_tmpf', 'min_tmpf', 'dwpf', 'relh', 'sknt',
                   'pday', 'alti', 'drct']:
