df4c56ae5011a5c02b340fc11c8b2802b6c1cec2
akrherz <akrherz@iastate.edu>
2018-08-30 23:03:13 -0500

    add another station and use alti for data

--- pylib/services/meteobridge.py
+++ pylib/services/meteobridge.py
@@ -16,13 +16,18 @@ def handler(version, fields, environ):
     if not PROPS:
         sys.stderr.write("Loading properties()...\n")
         PROPS.update(get_properties())
-    if fields.get('key') != PROPS.get('meteobridge.key.OT0013'):
+    if fields.get('key') not in [
+            PROPS.get('meteobridge.key.OT0013'),
+            PROPS.get('meteobridge.key.OT0014')]:
         return json.dumps("BAD_KEY")
+    sid = ('OT0013'
+           if PROPS.get('meteobridge.key.OT0013') == fields.get('key')
+           else 'OT0014')
     now = datetime.datetime.utcnow()
     now = now.replace(tzinfo=pytz.UTC)
-    ob = Observation('OT0013', 'OT', now)
+    ob = Observation(sid, 'OT', now)
     for fname in ['tmpf', 'max_tmpf', 'min_tmpf', 'dwpf', 'relh', 'sknt',
-                  'pday', 'pres', 'drct']:
+                  'pday', 'alti', 'drct']:
         if fields.get(fname, 'M') == 'M':
             continue
         ob.data[fname] = float(fields.get(fname))
