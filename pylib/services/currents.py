b550928bcef11e92914fcd8a47bbe53e1c456247
akrherz <akrherz@iastate.edu>
2019-02-05 09:42:29 -0600

    currents API enhancements
     * support `event=` param to filter on non-null data
     * geojson output may be okay now
     * don't hard code network

--- pylib/services/currents.py
+++ pylib/services/currents.py
@@ -1,21 +1,20 @@
-"""Currents API endpoint supporing a number of calls
+"""Currents API endpoint supporting a number of calls
 
     /api/1/currents.txt?network=IA_ASOS
     /api/1/currents.txt?networkclass=COOP&wfo=DMX
     /api/1/currents.txt?state=IA
     /api/1/currents.txt?wfo=DMX
     /api/1/currents.txt?station=DSM&station=AMW
+    /api/1/currents.txt?event=ice_accretion_1hr
 """
 import os
 import tempfile
 import warnings
 
 import numpy as np
-from metpy.units import units, masked_array
 from pandas.io.sql import read_sql
 from geopandas import read_postgis
 from pyiem.util import get_dbconn
-from pyiem.meteorology import mcalc_feelslike
 # prevent warnings that may trip up mod_wsgi
 warnings.simplefilter('ignore')
 
@@ -34,12 +33,13 @@ WITH agg as (
     c.pday as ob_pday, c.pmonth as ob_pmonth,
     gust, mslp, pres,
     scond0, scond1, scond2, scond3, srad,
-    tsf0, tsf1, tsf2, tsf3, rwis_subf, raw, phour,
+    tsf0, tsf1, tsf2, tsf3, rwis_subf, raw, phour, feel,
+    ice_accretion_1hr, ice_accretion_3hr, ice_accretion_6hr,
     skyl1, skyc1, skyl2, skyc2, skyl3, skyc3, skyl4, skyc4, alti,
     array_to_string(wxcodes, ' ') as wxcodes,
     t.geom, ST_x(t.geom) as lon, ST_y(t.geom) as lat
     from current c JOIN stations t on (c.iemid = t.iemid) WHERE
-    REPLACEME and not t.metasite
+    REPLACEME not t.metasite
 )
     SELECT c.id as station, c.name, c.county, c.state, c.network,
     to_char(s.day, 'YYYY-mm-dd') as local_date, snow, snowd, snoww,
@@ -50,7 +50,8 @@ WITH agg as (
     ob_pday, ob_pmonth, s.pmonth as s_pmonth,
     max_sknt, max_gust, gust, mslp, pres,
     scond0, scond1, scond2, scond3, srad,
-    tsf0, tsf1, tsf2, tsf3, rwis_subf, raw, phour,
+    tsf0, tsf1, tsf2, tsf3, rwis_subf, raw, phour, feel,
+    ice_accretion_1hr, ice_accretion_3hr, ice_accretion_6hr,
     skyl1, skyc1, skyl2, skyc2, skyl3, skyc3, skyl4, skyc4, alti,
     wxcodes,
     geom,
@@ -70,27 +71,17 @@ WITH agg as (
 
 def get_mckey(fields):
     """What's the key for this request"""
-    return "%s_%s_%s_%s_%s" % (fields.get('network', ''),
-                               fields.get('networkclass', ''),
-                               fields.get('wfo', ''),
-                               fields.get('state', ''),
-                               ",".join(fields.getall('station')))
+    return "%s_%s_%s_%s_%s_%s" % (
+        fields.get('network', ''), fields.get('networkclass', ''),
+        fields.get('wfo', ''), fields.get('state', ''),
+        ",".join(fields.getall('station')), fields.get('event', '')
+    )
 
 
 def compute(df):
     """Compute other things that we can't easily do in the database"""
     # replace any None values with np.nan
     df.fillna(value=np.nan, inplace=True)
-    if df.empty:
-        df['feel'] = None
-        return
-    tmpf = masked_array(df['tmpf'].values, units('degF'),
-                        mask=df['tmpf'].isnull())
-    dwpf = masked_array(df['dwpf'].values, units('degF'),
-                        mask=df['dwpf'].isnull())
-    smps = masked_array(df['sknt'].values, units('knots'),
-                        mask=df['sknt'].isnull())
-    df['feel'] = mcalc_feelslike(tmpf, dwpf, smps).to(units('degF')).magnitude
     # contraversy here, drop any columns that are all missing
     # df.dropna(how='all', axis=1, inplace=True)
 
@@ -98,34 +89,36 @@ def compute(df):
 def handler(_version, fields, _environ):
     """Handle the request, return dict"""
     fmt = fields.get("_format", "json")
-    if fmt == 'geojson':
-        raise ValueError("geojson is not implemented")
-    network = fields.get('network', 'IA_ASOS')[:32]
+    network = fields.get('network', '')[:32]
     networkclass = fields.get('networkclass', '')[:32]
     wfo = fields.get('wfo', '')[:4]
     state = fields.get('state', '')[:2]
+    event = fields.get('event')
     station = fields.getall('station')
     pgconn = get_dbconn('iem')
     if station:
         params = (tuple(station), )
         sql = SQL.replace("REPLACEME",
-                          "t.id in %s")
+                          "t.id in %s and")
     elif networkclass != '' and wfo != '':
         params = (wfo, networkclass)
         sql = SQL.replace("REPLACEME",
-                          "t.wfo = %s and t.network ~* %s")
+                          "t.wfo = %s and t.network ~* %s and")
     elif wfo != '':
         params = (wfo, )
         sql = SQL.replace("REPLACEME",
-                          "t.wfo = %s")
+                          "t.wfo = %s and")
     elif state != '':
         params = (state, )
         sql = SQL.replace("REPLACEME",
-                          "t.state = %s")
-    else:
+                          "t.state = %s and")
+    elif network != '':
         sql = SQL.replace("REPLACEME",
-                          "t.network = %s")
+                          "t.network = %s and")
         params = (network, )
+    else:
+        sql = SQL.replace("REPLACEME", "")
+        params = []
 
     if fmt == 'geojson':
         df = read_postgis(sql, pgconn, params=params,
@@ -133,8 +126,10 @@ def handler(_version, fields, _environ):
     else:
         df = read_sql(sql, pgconn, params=params, index_col='station')
         df.drop('geom', axis=1, inplace=True)
+    if event is not None and event in df.columns:
+        df = df[df[event].notna()]
     compute(df)
-    (tmpfd, tmpfn) = tempfile.mkstemp()
+    (tmpfd, tmpfn) = tempfile.mkstemp(text=True)
     os.close(tmpfd)
     if fmt == 'txt':
         df.to_csv(tmpfn, index=True)
@@ -142,7 +137,6 @@ def handler(_version, fields, _environ):
         # Implement our 'table-schema' option
         return df
     elif fmt == 'geojson':
-        # does not work https://github.com/Toblerity/Fiona/issues/409
         df.to_file(tmpfn, driver="GeoJSON")
 
     return open(tmpfn).read()
