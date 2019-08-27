9bff1c82eb594170a10b1d9f4a3fba0b9dd2624f
akrherz <akrherz@iastate.edu>
2019-08-27 09:22:41 -0500

    add CFS forecast into drydown akrherz/iem#179

--- pylib/services/drydown.py
+++ pylib/services/drydown.py
@@ -1,12 +1,15 @@
 """Provide data to support drydown app."""
 import warnings
+import datetime
+import os
 import json
 from io import StringIO
 
 from metpy.units import units
+from metpy.calc import relative_humidity_from_dewpoint
 from pandas.io.sql import read_sql
-from pyiem.util import get_dbconn
-from pyiem.iemre import get_gid
+from pyiem.util import get_dbconn, ncopen
+from pyiem.iemre import get_gid, find_ij, daily_offset
 # prevent warnings that may trip up mod_wsgi
 warnings.simplefilter('ignore')
 
@@ -19,6 +22,58 @@ def get_mckey(fields):
     return "%s_%s" % (
         fields.get('lat', ''), fields.get('lon', '')
     )
+
+
+def append_cfs(res, lon, lat):
+    """Append on needed CFS data."""
+    gridx, gridy = find_ij(lon, lat)
+    lastyear = max(res['data'].keys())
+    thisyear = datetime.date.today().year
+    if lastyear != thisyear:
+        # We don't have any data yet for this year, so we add some
+        res['data'][thisyear] = {
+            'dates': [],
+            'high': [],
+            'low': [],
+            'rh': []
+        }
+        lastdate = datetime.date(thisyear, 8, 31)
+    else:
+        lastdate = datetime.datetime.strptime(
+            res['data'][thisyear]['dates'][-1], '%Y-%m-%d'
+        ).date()
+    # go find the most recent CFS 0z file
+    valid = datetime.date.today()
+    attempt = 0
+    while True:
+        testfn = valid.strftime(
+            "/mesonet/data/iemre/cfs_%Y%m%d00.nc"
+        )
+        if os.path.isfile(testfn):
+            break
+        valid -= datetime.timedelta(hours=24)
+        attempt += 1
+        if attempt > 9:
+            return
+    nc = ncopen(testfn)
+    high = (
+        nc.variables['high_tmpk'][:, gridy, gridx] * units.degK).to(
+            units.degF).m
+    low = (
+        nc.variables['low_tmpk'][:, gridy, gridx] * units.degK).to(
+            units.degF).m
+    # RH hack
+    rh = relative_humidity_from_dewpoint(
+        high * units.degF, low * units.degF).m * 100.
+    entry = res["data"][thisyear]
+    tidx = daily_offset(lastdate)
+    for i in range(tidx, 365):
+        lts = datetime.date(thisyear, 1, 1) + datetime.timedelta(days=i)
+        if lts.month in [9, 10]:
+            entry['dates'].append(lts.strftime("%Y-%m-%d"))
+            entry['high'].append(int(high[i]))
+            entry['low'].append(int(low[i]))
+            entry['rh'].append(int(rh[i]))
 
 
 def handler(_version, fields, _environ):
@@ -47,6 +102,7 @@ def handler(_version, fields, _environ):
             'high': df2['max_tmpf'].values.astype('i').tolist(),
             'low': df2['min_tmpf'].values.astype('i').tolist(),
             'rh': df2['avg_rh'].values.astype('i').tolist()}
+    append_cfs(res, lon, lat)
     sio = StringIO()
     json.dump(res, sio)
     sio.seek(0)
