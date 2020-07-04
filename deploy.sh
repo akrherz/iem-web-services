337d3ab4b3d43e344412bc4cab611a67a0d5551b
akrherz <akrherz@iastate.edu>
2020-07-04 14:32:34 -0500

    use gunicorn for runtime

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 # Fire up our server
 export PYTHONPATH=$(pwd)/src
 
-uvicorn --host 0.0.0.0 --port 8000 app.main:app &
+# https://www.uvicorn.org/deployment/
+# Something (likely pygrib) leaks memory, so we don't let an individual worker
+# run for too long
+# --reload does not seem to work, shrug
+
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 \
+ --max-requests 500 --preload --reload app.main:app &
