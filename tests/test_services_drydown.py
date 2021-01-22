c6af6437b8375b2c6d0a4457abf6f7c5268f9350
akrherz <akrherz@iastate.edu>
2021-01-22 09:12:23 -0600

    improve robustness of CFS merge in drydown
    fixes #46

--- tests/test_services_drydown.py
+++ tests/test_services_drydown.py
@@ -1,10 +1,33 @@
 """Test the drydown service."""
+# local
+import os
+from datetime import timedelta, datetime
 
+# third party
 from fastapi.testclient import TestClient
 
+# local
 from iemws.main import app
+from iemws.services.drydown import append_cfs
 
 client = TestClient(app)
+
+
+def test_append_cfs():
+    """Test that we can append CFS data."""
+    res = {"data": {2021: {"dates": ["2021-11-01"]}}}
+    assert append_cfs(res, -95, 42) is None
+    # Create an empty netcdf to exercise more API
+    fn = (datetime.now() - timedelta(days=2)).strftime(
+        "/mesonet/data/iemre/cfs_%Y%m%d00.nc"
+    )
+    should_delete = not os.path.isfile(fn)
+    if should_delete:
+        with open(fn, "w") as fh:
+            fh.write("BAH")
+    assert append_cfs(res, -95, 42) is None
+    if should_delete:
+        os.unlink(fn)
 
 
 def test_basic():
