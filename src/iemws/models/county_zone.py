6e02cb8f4688424064d1734c2b3b7a8e46de4269
akrherz <akrherz@iastate.edu>
2022-01-05 09:25:45 -0600

    add service for VTEC WaWA by county/zone

---
+++ src/iemws/models/county_zone.py
@@ -0,0 +1,30 @@
+"""Models for county_zone API."""
+# pylint: disable=no-name-in-module
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class DataItem(BaseModel):
+    """Data Schema."""
+
+    utc_product_issue: str = Field(..., title="UTC Timestamp of Text Product")
+    utc_issue: str = Field(..., title="UTC Timestamp of Issue")
+    utc_expire: str = Field(..., title="UTC Timestamp of Expire")
+    ph_sig: str = Field(
+        ...,
+        title="Convienece combined phenomena and significance",
+    )
+    wfo: str = Field(..., title="Three-letter WFO/CWA Identifier")
+    eventid: int = Field(..., title="VTEC Event ID")
+    phenomena: str = Field(..., title="VTEC Phenomena Identifier")
+    significance: str = Field(..., title="VTEC Significance Identifier")
+    ugc: str = Field(..., title="Six-Character UGC Code")
+    nws_color: str = Field(..., title="NWS HEX Color Code with hash prefix")
+    event_label: str = Field(..., title="English Name of Event")
+
+
+class RootSchema(BaseModel):
+    """The schema used by this service."""
+
+    data: List[DataItem]
---
+++ src/iemws/services/vtec/county_zone.py
@@ -0,0 +1,74 @@
+"""NWS VTEC Watch/Warning/Advisories (WaWA) by County/Zone.
+
+This service provides VTEC-enabled NWS Watch, Warnings, and Advisories that
+are specific to counties/parishes and zones.  There is a seperate service
+for storm based / polygon VTEC events.  For the GeoJSON output, the returned
+geometries are not the greatest due to a whole host of reasons.  The
+attributes do contain the 6-character UGC codes, so joining that to a
+higher resolution dataset may be necessary for your visualization purposes.
+
+"""
+from datetime import datetime, timezone
+
+from geopandas import read_postgis
+from fastapi import Query, APIRouter
+from pyiem.util import utc
+from pyiem.nws.vtec import NWS_COLORS, get_ps_string
+from ...models import SupportedFormats
+from ...models.county_zone import RootSchema
+from ...util import get_dbconn, deliver_df
+
+router = APIRouter()
+
+
+def handler(valid):
+    """Handler"""
+    pgconn = get_dbconn("postgis")
+    if valid is None:
+        valid = utc()
+    valid = valid.replace(tzinfo=timezone.utc)
+
+    # Oh boy, the concept of "valid" is tough for in the future watches.
+    df = read_postgis(
+        """
+        SELECT
+        product_issue at time zone 'UTC' as utc_product_issue,
+        issue at time zone 'UTC' as utc_issue,
+        expire at time zone 'UTC' as utc_expire,
+        w.phenomena || '.' || w.significance as ph_sig,
+        w.wfo, eventid, phenomena, significance, w.ugc,
+        null as nws_color, null as event_label,
+        u.simple_geom as geom
+        from warnings w JOIN ugcs u on (w.gid = u.gid) WHERE
+        w.product_issue <= %s and w.expire > %s
+        ORDER by w.product_issue ASC
+        """,
+        pgconn,
+        geom_col="geom",
+        params=(valid, valid),
+        index_col=None,
+    )
+    df["nws_color"] = df["ph_sig"].apply(NWS_COLORS.get)
+    df["event_label"] = df["ph_sig"].apply(
+        lambda x: get_ps_string(*x.split("."))
+    )
+    return df
+
+
+@router.get(
+    "/vtec/county_zone.{fmt}",
+    description=__doc__,
+    response_model=RootSchema,
+)
+def service(
+    fmt: SupportedFormats,
+    valid: datetime = Query(
+        None, description="Return events that are valid at this time."
+    ),
+):
+    """Replaced above."""
+    df = handler(valid)
+    return deliver_df(df, fmt)
+
+
+service.__doc__ = __doc__
