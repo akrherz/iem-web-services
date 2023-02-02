01ecbcbadcef8e76d3f7290c233141452504b822
akrherz <akrherz@iastate.edu>
2023-02-01 22:34:49 -0600

    fix date type

--- src/iemws/services/nws/wpc_national_hilo.py
+++ src/iemws/services/nws/wpc_national_hilo.py
@@ -46,7 +46,8 @@ def handler(state, year):
     df = read_sql(
         text(
             f"""
-        select date, station, name, n_x, value,
+        select to_char(date, 'YYYY-MM-DD') as date,
+        station, name, n_x, value,
         to_char(sts at time zone 'UTC', 'YYYY-MM-DDThh24:MI:SSZ')
             as period_start,
         to_char(ets at time zone 'UTC', 'YYYY-MM-DDThh24:MI:SSZ')
