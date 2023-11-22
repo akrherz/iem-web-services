9d76c84ff1c1ebd5e4da14d9c809bc8f85fc1036
akrherz <akrherz@iastate.edu>
2023-11-21 19:22:28 -0600

    mnt: improve obhistory coverage

--- tests/test_services_obhistory.py
+++ tests/test_services_obhistory.py
@@ -1,9 +1,48 @@
 """Test the obhistory service."""
+from datetime import date
 
 from fastapi.testclient import TestClient
 from iemws.main import app
 
 client = TestClient(app)
+
+
+def test_404():
+    """Test that this generates a 404"""
+    req = client.get("/obhistory.json?station=AMW&network=AI&date=2000-01-01")
+    assert req.status_code == 404
+
+
+def test_asos_archive():
+    """Test that this works"""
+    req = client.get(
+        "/obhistory.json?station=AMW&network=IA_ASOS&date=2000-01-01"
+    )
+    assert req.status_code == 200
+
+
+def test_isusm_archive():
+    """Test that this works"""
+    req = client.get(
+        "/obhistory.json?station=BOOI4&network=ISUSM&date=2000-01-01"
+    )
+    assert req.status_code == 200
+
+
+def test_rwis_archive():
+    """Test that works"""
+    req = client.get(
+        "/obhistory.json?station=RAMI4&network=IA_RWIS&date=2000-01-01"
+    )
+    assert req.status_code == 200
+
+
+def test_today():
+    """Test a request for today's data."""
+    req = client.get(
+        f"/obhistory.json?date={date.today():%Y-%m-%d}&station=AMW&network=AI"
+    )
+    assert req.status_code == 200
 
 
 def test_basic():
