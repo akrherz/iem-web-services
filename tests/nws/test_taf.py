88327282410946f7c88a47e97a4ab16c22bdfc1e
akrherz <akrherz@iastate.edu>
2023-11-21 19:09:05 -0600

    mnt: improve coverage

--- tests/nws/test_taf.py
+++ tests/nws/test_taf.py
@@ -8,6 +8,12 @@ client = TestClient(app)
 
 def test_basic():
     """Test basic calls."""
-    req = client.get("/nws/taf.json")
+    req = client.get("/nws/taf.json?station=KDSM")
     res = req.json()
     assert res is not None
+
+
+def test_text():
+    """Test basic calls."""
+    req = client.get("/nws/taf.txt?station=KDSM")
+    assert req.status_code == 200
