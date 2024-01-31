835e9420cc7d2bccf527f1981ca65e5fbb4ef5c2
akrherz <akrherz@iastate.edu>
2024-01-31 13:39:52 -0600

    test: add coop to testdata load

--- testdata/load.sh
+++ testdata/load.sh
@@ -2,7 +2,7 @@
 
 psql -v "ON_ERROR_STOP=1" -f _postgis_ugcs.sql postgis
 
-for db in afos hads postgis mesosite uscrn
+for db in afos coop hads postgis mesosite uscrn
 do
     for fn in $(ls ${db}*.sql)
     do
