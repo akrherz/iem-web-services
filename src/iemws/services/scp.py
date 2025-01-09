a7c1b84a1d4aab84c2c6dcf1e30a43ca0da5d9ab
akrherz <akrherz@iastate.edu>
2025-01-09 13:07:04 -0600

    ✨ Add tz= support to daily SCP service
    refs akrherz/iem#1062

--- src/iemws/services/scp.py
+++ src/iemws/services/scp.py
@@ -9,10 +9,10 @@ the field ``mid_1`` represents the mid value from the Goes East Sounder. The
 given site may have 1 or more of those 3 potential options."""
 
 import datetime
+from zoneinfo import ZoneInfo
 
 import pandas as pd
 from fastapi import APIRouter, Query
-from pyiem.util import utc
 from sqlalchemy import text
 
 from ..util import deliver_df, get_sqlalchemy_conn
@@ -20,36 +20,41 @@ from ..util import deliver_df, get_sqlalchemy_conn
 router = APIRouter()
 
 
-def handler(station, date):
+def handler(station, date, tz: str):
     """Handle the request, return dict"""
     station = f"K{station}" if len(station) == 3 else station
     station3 = station[1:] if station.startswith("K") else station
-    sts = utc(date.year, date.month, date.day, 0, 0)
+    tzinfo = ZoneInfo(tz)
+    sts = datetime.datetime(date.year, date.month, date.day, tzinfo=tzinfo)
     ets = sts + datetime.timedelta(hours=24)
     with get_sqlalchemy_conn("asos") as dbconn:
         # Get METARs
         obs = pd.read_sql(
             text("""
-            SELECT valid at time zone 'UTC' as utc_valid, metar, skyc1, skyl1,
+            SELECT valid at time zone 'UTC' as utc_valid,
+            valid at time zone :tz as local_valid,
+            metar, skyc1, skyl1,
             skyc2, skyl2, skyc3, skyl3, skyc4, skyl4
             from alldata where station = :station3 and valid >= :sts
             and valid < :ets and report_type != 1 ORDER by valid ASC
             """),
             dbconn,
             index_col=None,
-            params={"station3": station3, "sts": sts, "ets": ets},
+            params={"station3": station3, "sts": sts, "ets": ets, "tz": tz},
         )
         # Get SCP
         scp = pd.read_sql(
             text("""
-            SELECT valid at time zone 'UTC' as utc_scp_valid, mid, high,
+            SELECT valid at time zone 'UTC' as utc_scp_valid,
+            valid at time zone :tz as local_scp_valid,
+            mid, high,
             cldtop1, cldtop2, eca, source from scp_alldata
             where station = :station and valid >= :sts
             and valid < :ets ORDER by valid ASC
                  """),
             dbconn,
             index_col=None,
-            params={"station": station, "sts": sts, "ets": ets},
+            params={"station": station, "sts": sts, "ets": ets, "tz": tz},
         )
     # Figure out how many unique sources we have
     df = None
@@ -95,10 +100,11 @@ def handler(station, date):
 )
 def service(
     station: str = Query(..., max_length=5, min_length=3),
-    date: datetime.date = Query(..., description="UTC date of interest"),
+    date: datetime.date = Query(..., description="Date of interest"),
+    tz: str = Query("UTC", description="Timezone to report timestamps in"),
 ):
     """Replaced above by __doc__."""
-    df = handler(station, date)
+    df = handler(station, date, tz)
     return deliver_df(df, "json")
 
 
