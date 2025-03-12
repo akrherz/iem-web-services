1bbdce7598fd15af7435f3a6c59ee1c546d0f256
akrherz <akrherz@iastate.edu>
2025-03-12 14:16:12 -0500

    ✨ Allow raobs_by_year to return all (for CSV case)

--- src/iemws/services/raobs_by_year.py
+++ src/iemws/services/raobs_by_year.py
@@ -1,57 +1,77 @@
-"""List of Sounding Parameters by Year.
+"""List of Sounding Station Parameters.
 
-This service provides IEM computed sounding parameters for a given site
-and year."""
+This service provides IEM computed sounding parameters for a given station
+and optional year.  If you do not specify a year, the service will only
+work for CSV output as the JSON is too large!"""
 
 from datetime import date
+from typing import Optional
 
-from fastapi import APIRouter, Query
-from pandas.io.sql import read_sql
+import pandas as pd
+from fastapi import APIRouter, HTTPException, Query
+from pyiem.database import sql_helper
 from pyiem.network import Table as NetworkTable
 from pyiem.util import utc
-from sqlalchemy import text
 
+from ..models import SupportedFormatsNoGeoJSON
 from ..util import deliver_df, get_sqlalchemy_conn
 
 router = APIRouter()
 
 
-def handler(station, year):
+def handler(station: str, year: Optional[int]) -> pd.DataFrame:
     """Handle the request, return dict"""
-    stations = [station]
+    params = {
+        "station": station,
+        "sts": None,
+        "ets": None,
+    }
+    station_limiter = "station = :station"
     if station.startswith("_"):
+        station_limiter = "station = ANY(:station)"
         nt = NetworkTable("RAOB", only_online=False)
-        stations = nt.sts[station]["name"].split("--")[1].strip().split()
+        params["station"] = (
+            nt.sts[station]["name"].split("--")[1].strip().split()
+        )
 
-    sts = utc(year, 1, 1)
-    ets = utc(year + 1, 1, 1)
+    time_limiter = ""
+    if year is not None:
+        time_limiter = " and valid >= :sts and valid < :ets"
+        params["sts"] = utc(year, 1, 1)
+        params["ets"] = utc(year + 1, 1, 1)
     with get_sqlalchemy_conn("raob") as pgconn:
-        df = read_sql(
-            text(
-                "SELECT * from raob_flights where station = ANY(:ids) "
-                "and valid >= :sts and valid < :ets ORDER by valid ASC"
+        df = pd.read_sql(
+            sql_helper(
+                """
+    SELECT * from raob_flights where {station_limiter}
+    {time_limiter} ORDER by valid ASC
+    """,
+                station_limiter=station_limiter,
+                time_limiter=time_limiter,
             ),
             pgconn,
-            params={"ids": stations, "sts": sts, "ets": ets},
+            params=params,
         )
 
     return df
 
 
 @router.get(
-    "/raobs_by_year.json",
+    "/raobs_by_year.{fmt}",
     description=__doc__,
     tags=[
         "nws",
     ],
 )
 def nwstext_service(
+    fmt: SupportedFormatsNoGeoJSON,
     station: str = Query(..., max_length=4, min_length=4),
-    year: int = Query(..., ge=1947, le=date.today().year),
+    year: Optional[int] = Query(None, ge=1947, le=date.today().year),
 ):
     """Replaced above by __doc__."""
-    df = handler(station, year)
-    return deliver_df(df, "json")
+    if fmt == "json" and year is None:
+        raise HTTPException(status_code=422, detail="JSON requires a year set")
+    return deliver_df(handler(station, year), fmt)
 
 
 nwstext_service.__doc__ = __doc__
--- tests/test_services_raobs_by_year.py
+++ tests/test_services_raobs_by_year.py
@@ -9,6 +9,18 @@ from iemws.main import app
 client = TestClient(app)
 
 
+def test_without_year_json():
+    """Test support for no year provision."""
+    res = client.get("/raobs_by_year.json", params={"station": "KOAX"})
+    assert res.status_code == 422
+
+
+def test_without_year_csv():
+    """Test that this generated."""
+    res = client.get("/raobs_by_year.txt", params={"station": "KOAX"})
+    assert res.status_code == 200
+
+
 def test_first():
     """Test we can do things."""
     res = client.get(
