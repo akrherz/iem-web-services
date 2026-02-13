d473b1cd64162821e87347e3f67e1fde4aa2cd7c
akrherz <akrherz@iastate.edu>
2026-02-13 09:01:25 -0600

    ✨ Improve TAF services with admendment and archive
    refs akrherz/iem#1514

--- src/iemws/services/nws/taf_overview.py
+++ src/iemws/services/nws/taf_overview.py
@@ -1,14 +1,29 @@
 """Terminal Aerodome Forecast (TAF) Overview.
 
-This service provides an overview of all presently available and most recent
-TAF issuances as processed and parsed by the IEM.  The attributes for each
+This service provides metadata on available TAF forecasts within the
+IEM database.  The attributes for each
 forecast include references to API endpoints providing either the raw
 text (`text_href`) or JSON representation (`data_href`).  Additionally a
 forecast aggregate of `min_visibility` (miles) is provided.
+
+If you provide a ``station``, you can request up to a year's worth of
+TAF metadata.  If you provide no ``station``, you can request up to 10 days
+worth of TAF metadata between ``sts`` and ``ets``.  If you provide ``at``,
+then you will get the last TAF forecast for the provided station or all
+stations prior to that time. The search looks over a 10 day window.
+
+The ``seqnum`` value in the response is meant to order the forecasts for
+a given station with `1` representing the most recent TAF for the station.
 """
 
-from fastapi import APIRouter
-from geopandas import read_postgis
+from datetime import datetime, timedelta
+from typing import Annotated
+
+import geopandas as gpd
+from fastapi import APIRouter, HTTPException
+from pydantic import Field
+from pyiem.database import sql_helper
+from pyiem.util import utc
 
 from ...models import SupportedFormats
 from ...util import deliver_df, get_sqlalchemy_conn
@@ -16,19 +31,57 @@ from ...util import deliver_df, get_sqlalchemy_conn
 router = APIRouter()
 
 
-def handler():
+def handler(
+    station: str | None,
+    sts: datetime | None,
+    ets: datetime | None,
+    at: datetime | None,
+):
     """Handle the request."""
+    query_params = {
+        "station": station,
+        "sts": sts,
+        "ets": ets,
+    }
+    # 1. Provided station
+    station_limiter = "" if station is None else " and station = :station"
+    row_number_operator = "="
+    # 2a. Provided at
+    if at is not None:
+        query_params["sts"] = at - timedelta(days=10)
+        query_params["ets"] = at
+    # 2b. sts and ets are provided
+    elif sts is not None and ets is not None:
+        row_number_operator = ">="
+        if (ets - sts) > timedelta(days=366):
+            raise HTTPException(
+                status_code=422,
+                detail="Too large of a request, please limit to 1 year",
+            )
+        if station is None and (ets - sts) > timedelta(days=10):
+            raise HTTPException(
+                status_code=422,
+                detail="Too large of a request, please limit to 10 days",
+            )
+    # 3. No timestamps provided
+    else:
+        query_params["sts"] = utc() - timedelta(days=10)
+        query_params["ets"] = utc()
     with get_sqlalchemy_conn("asos") as pgconn:
-        df = read_postgis(
-            """
+        df = gpd.read_postgis(
+            sql_helper(
+                """
             with forecasts as (
                 select station, id, valid as issuance, product_id,
+                is_amendment,
                 row_number() OVER (PARTITION by station ORDER by valid DESC)
-                from taf where valid > now() - '24 hours'::interval and
-                valid < now()),
+                from taf where valid > :sts and valid <= :ets
+                {station_limiter}),
             agg as (
-                select station, id, issuance, product_id from forecasts
-                where row_number = 1),
+                select station, id, issuance, product_id, is_amendment,
+                row_number as seqnumber
+                from forecasts
+                where row_number {row_number_operator} 1),
             stinfo as (
                 select a.*, t.geom, t.name from agg a JOIN stations t on (
                     (case when substr(a.station, 1, 1) = 'K'
@@ -39,16 +92,22 @@ def handler():
                 from agg a JOIN taf_forecast t on
                 (a.id = t.taf_id) GROUP by a.id)
 
-            select s.station, s.product_id, s.geom, s.name,
+            select s.station, s.product_id, s.geom, s.name, s.is_amendment,
+            s.seqnumber,
             to_char(s.issuance at time zone 'UTC', 'YYYY-MM-DDThh24:MI:SSZ')
                 as utc_issued,
             ST_X(geom) as lon, ST_Y(geom) as lat,
             a.min_visibility from stinfo s JOIN agg2 a on (s.id = a.id)
+            order by station asc, seqnumber asc
             """,
+                station_limiter=station_limiter,
+                row_number_operator=row_number_operator,
+            ),
             pgconn,
             geom_col="geom",
             index_col=None,
-        )
+            params=query_params,
+        )  # type: ignore
     df["data_href"] = (
         "/api/1/nws/taf.json?station="
         + df["station"]
@@ -68,9 +127,21 @@ def handler():
 )
 def service(
     fmt: SupportedFormats,
+    station: Annotated[
+        str | None, Field(description="Four character ICAO")
+    ] = None,
+    sts: Annotated[
+        datetime | None, Field(description="Start time for query")
+    ] = None,
+    ets: Annotated[
+        datetime | None, Field(description="End time for query")
+    ] = None,
+    at: Annotated[
+        datetime | None, Field(description="Time to query prior to")
+    ] = None,
 ):
     """Replaced above."""
-    df = handler()
+    df = handler(station, sts, ets, at)
     return deliver_df(df, fmt)
 
 
--- tests/nws/test_taf_overview.py
+++ tests/nws/test_taf_overview.py
@@ -7,8 +7,41 @@ from iemws.main import app
 client = TestClient(app)
 
 
+def test_overview_at():
+    """Test a request at a given timestamp."""
+    resp = client.get("/nws/taf_overview.json?at=2023-01-01T00:00:00Z")
+    assert resp.status_code == 200
+
+
+def test_too_large_request():
+    """Test too big of a request."""
+    resp = client.get(
+        "/nws/taf_overview.json?sts=2022-01-01T00:00&ets=2022-11-01T00:00"
+    )
+    assert resp.status_code == 422
+
+
+def test_too_large_request_station():
+    """Test too big of a request."""
+    resp = client.get(
+        "/nws/taf_overview.json?sts=2022-01-01T00:00&ets=2024-01-01T00:00"
+        "&station=KDSM"
+    )
+    assert resp.status_code == 422
+
+
+def test_ok_large_station_request():
+    """Test a large station request."""
+    resp = client.get(
+        "/nws/taf_overview.json?sts=2022-01-01T00:00&ets=2022-12-01T00:00"
+        "&station=KDSM"
+    )
+    assert resp.status_code == 200
+
+
 def test_basic():
     """Test basic calls."""
-    req = client.get("/nws/taf_overview.geojson")
-    res = req.json()
+    resp = client.get("/nws/taf_overview.geojson")
+    res = resp.json()
     assert res is not None
+    assert resp.status_code == 200
