f24049950c4c2173851b01282270b5962320b0ef
akrherz <akrherz@iastate.edu>
2026-04-10 10:11:04 -0500

    ⚡️ Drop --reload usage in prod, bad idea

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ MAXREQUESTS=$2
 # https://www.uvicorn.org/deployment/
 # Something (likely pygrib) leaks memory, so we don't let an individual worker
 # run for too long
-# --preload can not be used with --reload
+# --reload is dangerous for prod and unstable
 # Shutdown with just kill <PID>, no core dumps?
 # jitter tries to keep all workers restarting at the same time
 # graceful-timeout is how long we wait for a worker to finish
@@ -27,6 +27,5 @@ gunicorn \
  -b 0.0.0.0:8000 \
  --max-requests $MAXREQUESTS \
  --max-requests-jitter 500 \
- --reload \
  --log-level warning \
  iemws.main:app 2>&1 | logger -p local1.notice --tag iemws &
