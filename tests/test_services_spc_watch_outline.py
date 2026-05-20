5b30a1d881b54bf7d66b51c98f023c3527bb698c
akrherz <akrherz@iastate.edu>
2026-05-20 16:02:57 -0500

    💚 Address code review

--- tests/test_services_spc_watch_outline.py
+++ tests/test_services_spc_watch_outline.py
@@ -5,8 +5,8 @@ from fastapi.testclient import TestClient
 
 def test_first(client: TestClient):
     """Test we can do things."""
-    res = client.get(
+    resp = client.get(
         "/spc_watch_outline.geojson", params={"valid": "2024-05-21T12:00:00Z"}
     )
-    j = res.json()
-    assert "features" in j
+    assert resp.status_code == 200
+    assert "features" in resp.json()
